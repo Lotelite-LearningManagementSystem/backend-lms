9e35f350efdf980de8d8f69a74529103506f6374
prashantjadon <bughunterprashant@gmail.com>
2025-01-31 11:24:52 +0530

    first commit

--- server.js
+++ server.js
@@ -18,7 +18,7 @@ app.use('/api/admin', require('./routes/adminRoutes'));
 app.use('/api/users', require('./routes/userRoutes'));
 app.use('/api/content', require('./routes/contentRoutes'));
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 1000;
 
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
